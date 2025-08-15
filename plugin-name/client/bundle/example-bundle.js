85d491172bf2ca264c21140decb928cfa8a4fcb6
David Allsop <50546855+medavidallsop@users.noreply.github.com>
2025-08-15 07:55:38 +0100

    Comment amend

--- plugin-name/client/bundle/example-bundle.js
+++ plugin-name/client/bundle/example-bundle.js
@@ -1,6 +1,6 @@
 /*
 Ensure you have any dependencies for this file installed via package.json
-Update webpack.config.js + package.json build/watch scripts if you change the example folder/file structure
+Update webpack.config.js and package.json build/watch scripts if you change the example folder/file structure
 */
 
 //import example from 'example'; // Import from a package
